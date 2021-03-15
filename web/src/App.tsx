94f36ae27aea761cb2ddba43857d62f16c8dada3
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-14 22:56:10 -0300

    landing tsx e css definid

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-
-      <button type="button">
-        Clica em mim 
-      </button>
-    </div>
+   <Landing />
   );
 }
 
