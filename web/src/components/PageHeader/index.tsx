88edcf8e373f62176bae69d1e3fb7b791c641988
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-15 00:36:05 -0300

    arrumando css TeacherList

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -20,7 +20,10 @@ const PageHeader: React.FC<PageHeaderProps> = (props) =>{
 
           <div className="header-content">
             <strong>{props.title}</strong>
+
+            {props.children}
           </div>
+
         </header>
   );
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -7,7 +7,24 @@ import './styles.css';
 function TeacherList() {
   return (
       <div id="page-teacher-list" className="container">
-        <PageHeader title="Este são os proffys disponíveis."/>
+        <PageHeader title="Este são os proffys disponíveis.">
+          <form id="search-teachers">
+            <div className="input-block">
+              <label htmlFor="subject">Matéria</label>
+              <input type="text"id="subject"/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="week_day">Dia da semana</label>
+              <input type="text"id="week_day"/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="time">Horas</label>
+              <input type="text"id="time"/>
+            </div>
+          </form>
+        </PageHeader>
       </div>
     )
 }
