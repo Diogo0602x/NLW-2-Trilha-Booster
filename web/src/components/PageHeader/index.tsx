86aafb00095c6509a064ea73e572d0207463ccb9
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-15 23:38:12 -0300

    css teacher list atualziado

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps {
   title: string;
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,35 @@ function TeacherList() {
             </div>
           </form>
         </PageHeader>
+
+        <main>
+          <article className="teacher-item">
+            <header>
+              <img src="https://avatars.githubusercontent.com/u/72995630?s=460&u=abddd331b434d99a65708d972b9a2b643d49baa8&v=4" alt="Diogo Sales "/>
+              <div>
+                <strong>Diogo Sales</strong>
+                <span>Química</span>
+              </div>
+            </header>
+
+            <p>
+              Entusiasta das melhores tecnologias de química avançada.
+              <br/><br/>
+              Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+
+            <footer>
+              <p>
+                Preço/hora
+                <strong>R$: 100,00</strong>
+              </p>
+              <button type="button">
+                <img src={whatsappIcon} alt="Whatsapp"/>
+                Entrar em contato
+              </button>
+            </footer>
+          </article>
+        </main>
       </div>
     )
 }
