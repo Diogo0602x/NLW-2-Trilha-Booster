684887156a5ecc55dc9a539975edc11e9706977f
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-16 11:40:48 -0300

    Ambiente preparado para usar com insomnia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,20 @@
 import express from 'express';
 
+const app = express();
 
-console.log('Hello World');
+//GET: Buscar ou listar uma informaç!ao
+//POST: Criar alguma nova informação
+//PUT: Atualizar uma infromação existente
+//DELETE: Deletar uam informação existente
+
+app.post('/users', (request, response) => {
+  const users = [
+    { name: 'Diogo', age: 19 },
+    { name: 'Vini', age: 21 },
+  ];
+
+  return response.json(users);
+});
+
+app.listen(3333);
+
