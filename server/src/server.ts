0f5120f72588347808ee52fed221a3ca447697d4
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-16 19:34:50 -0300

    README criado

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,18 +2,10 @@ import express from 'express';
 
 const app = express();
 
-//GET: Buscar ou listar uma informaç!ao
-//POST: Criar alguma nova informação
-//PUT: Atualizar uma infromação existente
-//DELETE: Deletar uam informação existente
+app.use(express.json());
 
-app.post('/users', (request, response) => {
-  const users = [
-    { name: 'Diogo', age: 19 },
-    { name: 'Vini', age: 21 },
-  ];
-
-  return response.json(users);
+app.use('/', (request, response) => {
+  return response.json({ message: 'Hello World '});
 });
 
 app.listen(3333);
