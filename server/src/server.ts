43aade3fe137d606c00934ab07f51717dbb4433f
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-18 12:02:20 -0300

    routes criado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/', (request, response) => {
-  return response.json({ message: 'Hello World '});
-});
+app.use(routes);
 
 app.listen(3333);
 
