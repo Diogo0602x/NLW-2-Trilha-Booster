4c612463ff133f8d701859001921b62c6af6699b
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-18 11:52:22 -0300

    migration 02 criada

--- server/src/database/migrations/01_create_classes.ts
+++ server/src/database/migrations/01_create_classes.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('classes', table => {
     table.increments('id').primary();
     table.string('subject').notNullable();
-    table.string('cost').notNullable();
+    table.decimal('cost').notNullable();
 
     table.integer('user_id')
     .notNullable()
