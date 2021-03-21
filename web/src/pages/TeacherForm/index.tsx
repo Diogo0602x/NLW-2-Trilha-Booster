944b582b5f64aedfa7f72534f464f5457306a1ac
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-21 18:26:15 -0300

    pasta input criado

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
+import './styles.css';
+
 import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-    <PageHeader title="Que incrível que você quer dar aulas."/>
+    <PageHeader 
+      title="Que incrível que você quer dar aulas."
+      description="O primeiro passo é preencher o formulário de inscrição"
+    />
+
+    <main>
+      <fieldset>
+        <legend>Seus Dados</legend>
+        
+        <div className="input-block">
+              <label htmlFor="name">Nome completo</label>
+              <input type="text"id="name"/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="avatar">Avatar</label>
+              <input type="text"id="avatar"/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text"id="whatsapp"/>
+            </div>
+      </fieldset>
+    </main>   
   </div>
   )
 }
