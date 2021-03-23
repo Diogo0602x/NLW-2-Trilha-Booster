0f16a1b1761a137ef5a0bc1b888341fb49b255c6
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-22 21:53:44 -0300

    Textarea index pronto

--- web/src/components/Textarea/index.tsx
+++ web/src/components/Textarea/index.tsx
@@ -1,18 +1,18 @@
-import React, { TextareaHTMLAttributes} from 'react';
+import React, { TextareaHTMLAttributes } from 'react';
 
-import './styles.css'; 
+import './styles.css';
 
-interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement>{
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string;
   label: string;
 }
 
-const Textarea: React.FC<TextareaProps> = ({label, name, ...rest}) => {
+const  Textarea: React.FC<TextareaProps> = ({ label, name, ...rest }) => {
   return (
     <div className="textarea-block">
       <label htmlFor={name}>{label}</label>
-      <textarea id={name} id={name} {...rest}/>
-    </div>
+      <textarea id={name} {...rest}/>
+     </div>
   );
 }
 
