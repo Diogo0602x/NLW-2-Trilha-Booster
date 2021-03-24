d4dd09e63999b93b823802d234282ee10d3a06dd
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-24 00:35:03 -0300

    scheduleItems começado

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -10,6 +10,11 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
+  const scheduleItems = [
+    { week_day: 0, from: "8:00 AM", to: "4:00PM" },
+    { week_day: 2, from: "10:00 AM", to: "6:00PM" },
+  ]
+
   return (
     <div id="page-teacher-form" className="container">
     <PageHeader 
@@ -29,7 +34,6 @@ function TeacherForm() {
 
       <fieldset>
         <legend>Sobre a aula</legend>
-
           <Select 
             name="subject" 
             label="Matéria"
@@ -48,6 +52,36 @@ function TeacherForm() {
             />
           
           <Input name="cost" label="Custa da sua hora por aula"/>
+      </fieldset>
+
+      <fieldset>
+        <legend>Horários disponíveis
+          <button type="button">
+            + Novo horário
+          </button>
+        </legend>
+        
+        {scheduleItems.map(scheduleItem => { 
+          return (
+            <div className="schedule-item">
+              <Select 
+                name="week_day" 
+                label="Dia da semana"
+                options={[ 
+                  { value: '0', label: 'Domingo' },
+                  { value: '1', label: 'Segunda-feira' },
+                  { value: '2', label: 'Terça-feira' },
+                  { value: '3', label: 'Quarta-feira'  },
+                  { value: '4', label: 'Quinta-feira' },
+                  { value: '5', label: 'Sexta-feira' },
+                  { value: '6', label: 'Sábado' },
+                ]}
+              />
+              <Input name="from" label="Das" type="time" />
+              <Input name="to" label="Até" type="time" />
+            </div>
+          );  
+        })}
       </fieldset>
 
       <footer>
