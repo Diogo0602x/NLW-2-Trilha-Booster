62324e5ab7f8b34c12fa07422a225dc9abb5e064
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-23 23:32:12 -0300

    values criados

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -5,23 +5,13 @@ import './styles.css';
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   name: string;
   label: string;
-  options: Array<{
-    value: string;
-    label: string;
-  }>;
 }
 
-const  Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
+const  Select: React.FC<SelectProps> = ({ label, name, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select value="" id={name} {...rest}>
-        <option value="" disabled hidden>Selecione uma opção</option>
-
-        {options.map(option => {
-          return <option key={option.value} value={option.value}>{option.label}</option>
-        })}
-      </select>
+      <select id={name} {...rest}/>
      </div>
   );
 }
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
 import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
-import Textarea from '../../components/Textarea';
 
 function TeacherForm() {
   return (
@@ -22,14 +24,30 @@ function TeacherForm() {
         <Input name="name" label="Nome completo"/>
         <Input name="avatar" label="Avatar"/>
         <Input name="whatsapp" label="Whatsapp"/>
+        <Textarea name="bio" label="Biografia" />
       </fieldset>
 
       <fieldset>
         <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria"/>
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes'},
+              { value: 'Biologia', label: 'Biologia'},
+              { value: 'Ciências', label: 'Ciências'},
+              { value: 'Educação física', label: 'Educação física'},
+              { value: 'Física', label: 'Física'},
+              { value: 'Geografia', label: 'Geografia'},
+              { value: 'História', label: 'História'},
+              { value: 'Matemática', label: 'Matemática'},
+              { value: 'Português', label: 'Português'},
+              { value: 'Química', label: 'Química'},
+            ]}
+            />
+          
           <Input name="cost" label="Custa da sua hora por aula"/>
-          <Textarea name="bio" label="Biografia" />
       </fieldset>
 
       <footer>
