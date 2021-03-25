5ae81488f380d45e753eee7ec21facde156fac18
Diogo Sales Teixeira <Diogo0602@users.noreply.github.com>
2021-03-24 21:50:47 -0300

    useState colocado nos Dados

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -10,6 +10,11 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [bio, setBio] = useState('');
+
   const [scheduleItems, setScheduleItems] = useState([
     { week_day: 0, from: '', to: '' }
   ]);
@@ -32,10 +37,30 @@ function TeacherForm() {
       <fieldset>
         <legend>Seus Dados</legend>
 
-        <Input name="name" label="Nome completo"/>
-        <Input name="avatar" label="Avatar"/>
-        <Input name="whatsapp" label="Whatsapp"/>
-        <Textarea name="bio" label="Biografia" />
+        <Input 
+          name="name" 
+          label="Nome completo" 
+          value={name}
+          onChange={(e) => { setName(e.target.value) }}
+        />
+        <Input 
+          name="avatar" 
+          label="Avatar"
+          value={avatar}
+          onChange={(e) => { setAvatar(e.target.value) }}
+        />
+        <Input 
+          name="whatsapp" 
+          label="Whatsapp"
+          value={whatsapp}
+          onChange={(e) => { setWhatsapp(e.target.value) }}
+        />
+        <Textarea 
+          name="bio" 
+          label="Biografia" 
+          value={bio}
+          onChange={(e) => { setBio(e.target.value) }}
+        />
       </fieldset>
 
       <fieldset>
